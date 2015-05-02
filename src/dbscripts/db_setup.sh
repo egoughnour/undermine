4d5ef8f5632ae6ab1f6e7c5704b95472ef8591d9
Erik Goughnour <e.goughnour@gmail.com>
2015-05-01 20:28:03 -0500

    Corrected script path
    CQL should finally be on right path

--- src/dbscripts/db_setup.sh
+++ src/dbscripts/db_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-export PRJ_SOURCE_DIRECTORY=$(cd ..; pwd)
-cqlsh -f ${PRJ_SOURCE_DIRECTORY}/dbscripts/CreateTestKeyspace.cql
+export PRJ_PARENT_DIR=$(cd ..; pwd)
+cqlsh -f ${PRJ_PARENT_DIR}/undermine/src/dbscripts/CreateTestKeyspace.cql
